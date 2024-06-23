9f0096281c0701e21098a7f3c7f9557280d2b46f
VenkateshSircilla123 <sircillavenkatesh0@gmail.com>
2024-06-23 22:27:33 +0530

    socket cors

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://threads-clone-backend.vercel.app",
+    origin: "https://threads-clone-backend.vercel.app/*",
     methods: ["GET", "POST"],
   },
 });
